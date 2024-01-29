10f9606c2a1dbd09624c26bcd431a7f0bb4b5784
mirkou <92599923+jmiccoDev@users.noreply.github.com>
2024-01-29 11:16:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,88 +1,77 @@
-// Create Web Server
+// Create web server
 
-// 1. Import Express
+// Import dependencies
 const express = require('express');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment.js');
 
-// 2. Create App
+// Create web server
 const app = express();
 
-// 3. Create Array
-const commentsByPostId = {};
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true });
 
-// 4. Middleware
-app.use(express.json());
-app.use(cors());
+// Configure web server
+app.use(bodyParser.json());
 
-// 5. Route Handler
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
-});
-
-// 6. Route Handler
-app.post('/posts/:id/comments', async (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
-
-    // Get comments from commentByPostId
-    const comments = commentsByPostId[req.params.id] || [];
-
-    // Add new comment
-    comments.push({ id: commentId, content, status: 'pending' });
-
-    // Update commentsByPostId
-    commentsByPostId[req.params.id] = comments;
-
-    // Emit event
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id,
-            status: 'pending'
-        }
-    });
-
-    // Send response
-    res.status(201).send(comments);
-});
-
-// 7. Route Handler
-app.post('/events', async (req, res) => {
-    console.log('Event Received:', req.body.type);
-
-    const { type, data } = req.body;
-
-    if (type === 'CommentModerated') {
-        const { postId, id, status, content } = data;
-
-        const comments = commentsByPostId[postId];
-
-        const comment = comments.find(comment => {
-            return comment.id === id;
-        });
-
-        comment.status = status;
-
-        await axios.post('http://localhost:4005/events', {
-            type: 'CommentUpdated',
-            data: {
-                id,
-                status,
-                postId,
-                content
-            }
-        });
+// GET /comments
+app.get('/comments', (req, res) => {
+  Comment.find({}, (err, comments) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(comments);
     }
-
-    res.send({});
+  });
 });
 
-// 8. Listen to Port
-app.listen(4001, () => {
-    console.log('Listening on 4001');
+// POST /comments
+app.post('/comments', (req, res) => {
+  const comment = new Comment(req.body);
+  comment.save((err, comment) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(comment);
+    }
+  });
 });
-    
+
+// GET /comments/:id
+app.get('/comments/:id', (req, res) => {
+  Comment.findById(req.params.id, (err, comment) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(comment);
+    }
+  });
+});
+
+// PUT /comments/:id
+app.put('/comments/:id', (req, res) => {
+  Comment.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, comment) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(comment);
+    }
+  });
+});
+
+// DELETE /comments/:id
+app.delete('/comments/:id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.id, (err, comment) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json({ message: 'Comment successfully deleted' });
+    }
+  });
+});
+
+// Start web server
+app.listen(3000, () => {
+  console.log('Comments API listening on port 3000!');
+}); 
